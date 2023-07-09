c0cc278456295837127fc6d2bc2f6e62499c208d
hemu kumar <hemuk4631@gmail.com>
2023-07-09 13:09:08 +0530

    inital

--- config/database.js
+++ config/database.js
@@ -4,6 +4,7 @@ export const connectDB = async () => {
   try {
     const { connection } = await mongoose.connect(process.env.MONGO_URI);
     console.log(`Mongodb connected with ${connection.host}`);
+    
   } catch (error) {
     console.log(error.message);
   }
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 import app from "./app.js";
 import { connectDB } from "./config/database.js";
 
+
 // Start the server
 app.listen(process.env.PORT, () => {
   connectDB();
   console.log(`server is working on port: ${process.env.PORT}`);
+
 });
